cc7185fd056900a2065a7d0bbb45e71d450aa2de
kengo922 <kengo.matsuno.922@gmail.com>
2020-07-11 05:31:51 +0900

    environmentsファイルが消えていたため、作成した

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :product do
-    
+    text              {"落ちろよおおおおおおおおおおお"}
+    image             {"tadashi.mp4"}
+    category_id       {10}
+    pen_history_id    {3}
+
   end
 end
+
+
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,68 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#create' do #describeは、ネストして作成する事が出来る。「productクラスにあるcreateメソッドをテストするまとまり」であることを示している。
+    #describeとdoの間にメソッド名を書く際は#をつけるのが慣習らしい。
+
+    it "textが500文字以内でないと登録できないこと" do
+      product = build(:product, text: "a" * 501)
+      product.valid?
+      expect(product).to be_invalid
+    end
+
+    it "imageがない場合は登録できないこと" do
+      product = build(:product, image: nil)
+      product.valid?
+      expect(product.errors[:image]).to include("を入力してください")
+    end
+
+    it "textがない場合は登録できないこと" do   #it ~ doの間はそのexampleでどんなテストをしているか説明文を記入する
+      product = build(:product, text: nil)
+      product.valid? #この記述(valid?)により「バリデーションにより保存ができない状態であるか」を確認する。
+      expect(product.errors[:text]).to include("を入力してください")
+      #errors ： valid?メソッドを利用したインスタンスに対してerrorsメソッドを利用すると、「バリデーションにより保存ができない状態である場合なぜできないのか」を確認する事が出来る。
+      #includeマッチャ : 引数の値(上記の場合は、(:product, name: nil)が引数)がexpectの引数である配列に含まれているかをチェックすることができる。
+      #今回の場合、「nameが空の場合は名前を入力してくださいというエラーが出るはずだ」ということがわかっている為、include("を入力してください")のように書く事が出来る。
+    end
+
+    it "pen_history_idがない場合は登録できないこと" do
+      product = build(:product, pen_history_id: nil)
+      product.valid?
+      expect(product.errors[:pen_history_id]).to include("を入力してください")
+    end
+
+    it "category_idがない場合は登録できないこと" do
+      product = build(:product, category_id: nil)
+      product.valid?
+      expect(product.errors[:category_id]).to include("を入力してください")
+    end
+  end
+
+  describe '#update' do
+
+    it "textが500文字以内でないと登録できないこと" do
+      product = build(:product, text: "a" * 501)
+      product.valid?
+      expect(product).to be_invalid
+    end
+
+    it "textがない場合は登録できないこと" do
+      product = build(:product, text: nil)
+      product.valid?
+      expect(product.errors[:text]).to include("を入力してください")
+    end
+
+    it "pen_history_idがない場合は登録できないこと" do
+      product = build(:product, pen_history_id: nil)
+      product.valid?
+      expect(product.errors[:pen_history_id]).to include("を入力してください")
+    end
+
+    it "category_idがない場合は登録できないこと" do
+      product = build(:product, category_id: nil)
+      product.valid?
+      expect(product.errors[:category_id]).to include("を入力してください")
+    end
+  end
 end
+
